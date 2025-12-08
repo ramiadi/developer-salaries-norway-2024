facffe7d4f2e00bc30522a9377a64ec0faa8ef33
ramiadi <ramin2004@hotmail.no>
2025-12-08 18:23:29 +0100

    geojson file

--- developer_salaries.py
+++ developer_salaries.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 
-# 1. LAST INN DINE DATA (erstatt px.data.election())
 df = pd.read_csv('salaries.csv', delimiter=',', encoding='utf-8')
 
+fylker = gpd.read_file(r'C:\Users\ramin\Github\developer-salaries-norway-2024\shapefile\gadm41_NOR_1.shp')
+fylker.plot(cmp = 'jet', edgecolor = 'black', column = 'fylker')
